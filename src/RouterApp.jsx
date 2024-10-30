6ba8f30f3911d730fb00b35aab5dd2f62ce5425a
zeru-program <justinebogor@gmail.com>
2024-10-30 14:50:47 +0700

    update

---
+++ src/RouterApp.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+
+function RouterApp() {
+  return (
+    <>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+      </Routes>
+    </Router>
+    </>
+  )
+}
+
+export default RouterApp
